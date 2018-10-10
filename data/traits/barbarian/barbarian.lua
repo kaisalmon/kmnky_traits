cb7c7837cd02c8a5e900906c3e11e31dc48c3672
Kai Salmon <kaisalmon@hotmail.com>
2018-10-10 20:12:51 +0200

    Swallows a bug

--- data/traits/barbarian/barbarian.lua
+++ data/traits/barbarian/barbarian.lua
@@ -25,8 +25,9 @@ function Barbarian:post_activate()
          local options = {}
          options.dont_drop_talisman = true
          options.skip_visual_effects = true
-         self._sv._entity:get_component('stonehearth:job'):promote_to('stonehearth:jobs:footman', options)
-
+         local able_to_be_footman = pcall(function()
+           self._sv._entity:get_component('stonehearth:job'):promote_to('stonehearth:jobs:footman', options)
+         end)
          local all_jobs = stonehearth.player:get_jobs(self._sv._entity:get_player_id())
          local allowed_jobs = {}
          for job_uri, _ in pairs(all_jobs) do
